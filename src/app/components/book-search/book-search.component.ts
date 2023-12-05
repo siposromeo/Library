0a53e2732086db01cc67afae41ac19d59df47db6
siposromeo <sipos.romeo.bence@students.jedlik.eu>
2023-12-05 08:23:58 +0100

    version 1

---
+++ src/app/components/book-search/book-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './book-search.component.html',
+  styleUrl: './book-search.component.css'
+})
+export class BookSearchComponent {
+
+}
